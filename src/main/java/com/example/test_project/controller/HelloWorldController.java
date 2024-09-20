bcf67000fbc92d8d82d63f8dad321a3364f5c3bf
mariyamango <de.msomkova@gmail.com>
2024-09-20 10:49:14 +0200

    Added api/hello/{name} implementation

--- src/main/java/com/example/test_project/controller/HelloWorldController.java
+++ src/main/java/com/example/test_project/controller/HelloWorldController.java
@@ -1,16 +1,23 @@
 package com.example.test_project.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 // http://localhost:8080/api/hello
+// http://localhost:8080/api/hello/{name}
 @RequestMapping("/api")
 public class HelloWorldController {
 
     @GetMapping("/hello")
     public String hello(){
         return "Hello, World!";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String helloName(@PathVariable("name") String name){
+        return "Hello, " + name + "!";
     }
 }
